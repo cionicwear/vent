e9817a8c492ca597fdd201d867aac54e1cc7638e
Jeremiah Robison <jerobi@gmail.com>
2020-06-14 18:18:21 +0100

    breakout sensors into their own files

--- sensor.py
+++ sensor.py
@@ -6,15 +6,11 @@ import logging
 
 import board
 import busio
-import bme680
-import d6f
 
-import adafruit_lps35hw as LPS
-import adafruit_ads1x15.ads1115 as ADS
-import adafruit_mprls as MPRLS
-from adafruit_ads1x15.analog_in import AnalogIn
 import rpi2c
 import valve
+
+from sensor_lps import PressureSensorLPS
 
 i2c_in = rpi2c.rpi_i2c(1)
 i2c_ex = rpi2c.rpi_i2c(3)
@@ -22,97 +18,6 @@ i2c_ex = rpi2c.rpi_i2c(3)
 VCO = 2.40256
 MAXPA = 4000
 
-class FlowSensorADS:
-    class Data:
-        def __init__(self):
-            self.rate = 0
-            
-    def __init__(self, channel=ADS.P0):
-        self.adc = ADS.ADS1115(i2c)
-        self.chan = AnalogIn(self.adc, channel)
-        self.data = self.Data()
-        
-    def read(self):
-        self.data.rate = self.chan.voltage
-
-class PressureSensorBME:
-    class Data:
-        def __init__(self):
-            self.pressure = 0
-            self.temperature = 0
-            self.humidity = 0
-            
-    def __init__(self):
-        try:
-            self.bme = bme680.BME680(bme680.I2C_ADDR_PRIMARY)
-        except IOError:
-            self.bme = bme680.BME680(bme680.I2C_ADDR_SECONDARY)
-        except Exception as e:
-            print(e)
-
-        self.data = self.Data()
-        self.bme.set_humidity_oversample(bme680.OS_1X)
-        self.bme.set_pressure_oversample(bme680.OS_1X)
-        self.bme.set_temperature_oversample(bme680.OS_1X)
-        self.bme.set_filter(bme680.FILTER_SIZE_1)
-        
-        #sensor.set_gas_status(bme680.DISABLE_GAS_MEAS)
-        #sensor.set_gas_heater_temperature(320)
-        #sensor.set_gas_heater_duration(150)
-        #sensor.select_gas_heater_profile(0)
-
-    def read(self):
-        if self.bme.get_sensor_data():
-            self.data.pressure = self.bme.data.pressure
-            self.data.temperature = self.bme.data.temperature
-            self.data.humidity = self.bme.data.humidity
-
-class FlowSensorD6F:
-    class Data:
-        def __init__(self):
-            self.rate = 0
-
-    def __init__(self, i2c):
-        self.flow = d6f.D6F70A(i2c)
-        self.data = self.Data()
-
-    def read(self):
-        self.data.rate = self.flow.read_flow()
-
-class PressureSensorLPS:
-    class Data:
-        def __init__(self):
-            self.pressure = 0
-            self.temperature = 0
-            self.humidity = 0
-
-    def __init__(self, i2c, address=0x5D):
-        self.lps = LPS.LPS35HW(i2c, address=address)
-        self.lps.data_rate = LPS.DataRate.RATE_75_HZ
-        #self.lps.low_pass_enabled = 1
-        self.data = self.Data()
-
-    def read(self):
-        # pressure reading in hPa convert to Pa
-        self.data.pressure = self.lps.pressure * 100 
-        self.data.temperature = self.lps.temperature
-
-    def zero_pressure(self):
-        self.lps.zero_pressure()
-
-class PressureSensorMPRLS:
-    class Data:
-        def __init__(self):
-            self.pressure = 0
-            self.temperature = 0
-            self.humidity = 0
-
-    def __init__(self, i2c, address=0x5D):
-        self.mprls = MPRLS.MPRLS(i2c)
-        self.data = self.Data()
-
-    def read(self):
-        self.data.pressure = self.mprls.pressure
 
 def check_spontaneous(pressure, breathing, assist):
     if pressure < -(assist) and breathing.value == 0:
